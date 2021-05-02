f8e94ed8954579e9ecb8e9f49d4f036f6ef9e67b
chetin4ever <hetan009@gmail.com>
2021-05-02 12:42:31 +0530

    using global style and inline style

--- dojo-blog/src/Navbar.js
+++ dojo-blog/src/Navbar.js
@@ -1,10 +1,20 @@
 const Navbar = () => {
   return (
-    <nav className='Navbar'>
+    <nav className='navbar'>
       <h1>Cejo Blog</h1>
       <div className='links'>
         <a href='/'>Home</a>
         <a href='/create'>New Blog</a>
+        {/* <a
+          href='/create'
+          style={{
+            color: "white",
+            backgroundColor: "#f1356d",
+            borderRadius: "10px",
+          }}
+        >
+          New Blog
+        </a> */}
       </div>
     </nav>
   )
