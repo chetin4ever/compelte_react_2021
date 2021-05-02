128a20626dbda15e2525c5562e4709b0f937c83e
chetin4ever <hetan009@gmail.com>
2021-05-02 11:45:25 +0530

    using dynamic value in compoent

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,10 +1,26 @@
 import "./App.css"
 
 function App() {
+  const title = "Welcome to new blog"
+  const likes = "50"
+  const person = {
+    name: "chetan",
+    age: 32,
+  }
+  const link = "http://www.google.com"
   return (
     <div className='App'>
       <div className='content'>
-        <h1>App Component </h1>
+        {/* printing javascript */}
+        <h1>{title}</h1>
+        <p>likes {likes}</p>
+        {/* can't print oobject directly */}
+        {/* {person}   */} {/* not valid */}
+        {person.name}
+        <p>{Math.random() * 10}</p>
+        <p>[1,2,3,4,5]</p>
+        {/* dyanamic link */}
+        <a href={link}>google site</a>
       </div>
     </div>
   )
