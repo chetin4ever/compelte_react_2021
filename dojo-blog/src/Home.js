25cd48a3163f6d62e87b252acfa71239dc59607f
chetin4ever <hetan009@gmail.com>
2021-05-02 14:21:19 +0530

    make changes

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,17 +1,25 @@
+import { useState } from "react"
 const Home = () => {
+  const [name, setName] = useState("chetan")
+  const [age, setAge] = useState(25)
   const handleClick = () => {
     console.log("handle click")
   }
-  const handleClickAgain = (name, e) => {
+  const handleClickAgain = (name, age, e) => {
     console.log("hello", name)
     console.log(e.target)
+    setName(name)
+    setAge(age)
   }
   return (
     <div className='home'>
       <h1>Home Components</h1>
+      <p>
+        {name} is {age}
+      </p>
       <button onClick={handleClick}>click me</button>
-      <button onClick={(e) => handleClickAgain("chetan", e)}>
-        click me againn
+      <button onClick={(e) => handleClickAgain("mahajan", 25, e)}>
+        click me again
       </button>
     </div>
   )
