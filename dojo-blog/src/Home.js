dfc1b0187e642fb252fa4a820d7bc49918a9ba27
chetin4ever <hetan009@gmail.com>
2021-05-02 13:12:16 +0530

    adding event

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+  const handleClick = () => {
+    console.log("handle click")
+  }
+  const handleClickAgain = (name, e) => {
+    console.log("hello", name)
+    console.log(e.target)
+  }
   return (
     <div className='home'>
       <h1>Home Components</h1>
+      <button onClick={handleClick}>click me</button>
+      <button onClick={(e) => handleClickAgain("chetan", e)}>
+        click me again
+      </button>
     </div>
   )
 }
